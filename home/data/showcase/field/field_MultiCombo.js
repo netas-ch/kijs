14a294f2cc52f235fa909878d9284d0e1565d9db
Joel Kohler <jk@informatex.ch>
2023-08-11 12:24:00 +0200

    Vorschlag MultiCombo

---
+++ home/data/showcase/field/field_MultiCombo.js
@@ -0,0 +1,322 @@
+/* global kijs */
+
+window.home.sc = {};
+home.sc.field_MultiCombo = class home_sc_field_MultiCombo {
+
+
+    // --------------------------------------------------------------
+    // CONSTRUCTOR
+    // --------------------------------------------------------------
+    constructor(config={}) {
+        this._app = config.app;
+        this._content = null;
+    }
+
+
+
+    // --------------------------------------------------------------
+    // MEMBERS
+    // --------------------------------------------------------------
+    getContent() {
+        this._content = new kijs.gui.Panel({
+            caption: 'kijs.gui.field.MultiCombo',
+            scrollableY: 'auto',
+            cls: 'kijs-flexform',
+            style: {
+                flex: 1
+            },
+            innerStyle: {
+                padding: '10px'
+            },
+            headerElements: this._getHeaderElements(),
+            elements:[
+                {
+                    xtype: 'kijs.gui.Element',
+                    html: 'Minimalkonfiguration:'
+                },{
+                    xtype: 'kijs.gui.field.MultiCombo',
+                    label: 'Label',
+                    captionField: 'caption',
+                    valueField: 'value',
+                    data: [
+                        { caption: 'Apple', iconMap: 'kijs.iconMap.Fa.apple', value: 1},
+                        { caption: 'Linux', iconMap: 'kijs.iconMap.Fa.linux', value: 2},
+                        { caption: 'Windows', iconMap: 'kijs.iconMap.Fa.windows', value: 3}
+                    ],
+                    on: {
+                        focus:  console.log,
+
+                        blur:  console.log,
+                        change: console.log,
+                        input:  console.log,
+
+                        context: this
+                    }
+                },
+
+                {
+                    xtype: 'kijs.gui.Element',
+                    html: 'RPC',
+                    style: { margin: '10px 0 0 0'}
+                },{
+                    xtype: 'kijs.gui.field.MultiCombo',
+                    label: 'Label Rpc',
+                    captionField: 'caption',
+                    valueField: 'value',
+                    rpcLoadFn: 'multicombo.load',
+                    on: {
+                        focus:  console.log,
+
+                        blur:  console.log,
+                        change: console.log,
+                        input:  console.log,
+
+                        context: this
+                    }
+                }
+            ]
+        });
+
+        return this._content;
+    }
+
+    run() {
+
+    }
+
+
+    // PROTECTED
+    _callFunction(fnName) {
+        kijs.Array.each(this._content.elements, function(el) {
+            if (el instanceof kijs.gui.field.Field) {
+                el[fnName]();
+            }
+        }, this);
+    }
+
+    _getHeaderElements() {
+        return [
+            {
+                xtype: 'kijs.gui.field.Switch',
+                label: 'disabled',
+                on: {
+                    change: function(e) {
+                        this._content.innerDisabled = !!e.element.value;
+                    },
+                    context: this
+                }
+            },{
+                xtype: 'kijs.gui.field.Switch',
+                label: 'disableFlex',
+                on: {
+                    change: function(e) {
+                        this._updateProperty('disableFlex', e.element.value);
+                    },
+                    context: this
+                }
+            },{
+                xtype: 'kijs.gui.field.Switch',
+                label: 'labelHide',
+                on: {
+                    change: function(e) {
+                        this._updateProperty('labelHide', e.element.value);
+                    },
+                    context: this
+                }
+            },{
+                xtype: 'kijs.gui.field.Switch',
+                label: 'labelWidth = 120',
+                on: {
+                    change: function(e) {
+                        this._updateProperty('labelWidth', e.element.value ? 120 : null);
+                    },
+                    context: this
+                }
+            },{
+                xtype: 'kijs.gui.field.Switch',
+                label: 'readOnly',
+                on: {
+                    change: function(e) {
+                        this._updateProperty('readOnly', e.element.value);
+                    },
+                    context: this
+                }
+            },{
+                xtype: 'kijs.gui.field.Switch',
+                label: 'required',
+                on: {
+                    change: function(e) {
+                        this._updateProperty('required', e.element.value);
+                        this._callFunction('validate');
+                    },
+                    context: this
+                }
+            },{
+                xtype: 'kijs.gui.field.Switch',
+                label: 'showHelp',
+                on: {
+                    change: function(e) {
+                        let value = '';
+                        if (e.element.value) {
+                            value = 'Dies ist ein Hilfetext';
+                        }
+                        this._updateProperty('helpText', value);
+                    },
+                    context: this
+                }
+            },{
+                xtype: 'kijs.gui.field.Switch',
+                label: 'spinIconVisible',
+                value: true,
+                on: {
+                    change: function(e) {
+                        this._updateProperty('spinIconVisible', e.element.value);
+                    },
+                    context: this
+                }
+            },{
+                xtype: 'kijs.gui.field.Switch',
+                label: 'Debug-Buttons',
+                on: {
+                    change: function(e) {
+                        kijs.Array.each(this._content.elements, function(el) {
+                            if (el instanceof kijs.gui.field.Field) {
+                                if (e.value) {
+                                    el.add(new kijs.gui.Button({
+                                        name: 'showValue',
+                                        tooltip: 'value anzeigen',
+                                        iconMap: 'kijs.iconMap.Fa.eye',
+                                        on: {
+                                            click: function(e) {
+                                                kijs.gui.CornerTipContainer.show('value', '<pre style="border:1px solid #000">'+el.value+'</pre>');
+                                            },
+                                            context: this
+                                        }
+                                    }));
+                                    el.add(new kijs.gui.Button({
+                                        name: 'setValue',
+                                        tooltip: 'value neu setzen (value=value)',
+                                        iconMap: 'kijs.iconMap.Fa.pen',
+                                        on: {
+                                            click: function(e) {
+                                                let val = el.value;
+                                                el.value = val;
+                                                this._updateIsDirtyButton(el, e.value);
+                                            },
+                                            context: this
+                                        }
+                                    }));
+                                    el.add(new kijs.gui.Button({
+                                        name: 'resetValue',
+                                        tooltip: 'valuesReset',
+                                        iconMap: 'kijs.iconMap.Fa.arrow-rotate-left',
+                                        on: {
+                                            click: function(e) {
+                                                el.valuesReset();
+                                                this._updateIsDirtyButton(el, e.value);
+                                            },
+                                            context: this
+                                        }
+                                    }));
+                                    el.on('input', this.#onInputForIsDirty, this);
+                                } else {
+                                    el.remove([
+                                        el.down('showValue'),
+                                        el.down('setValue'),
+                                        el.down('resetValue')
+                                    ]);
+                                    
+                                    el.off('input', this.#onInputForIsDirty, this);
+                                }
+                                this._updateIsDirtyButton(el, e.value);
+                            }
+                        }, this);
+                    },
+                    context: this
+                }
+            },{
+                xtype: 'kijs.gui.Button',
+                caption: 'Validate',
+                on: {
+                    click: function(e) {
+                        this._callFunction('validate');
+                    },
+                    context: this
+                }
+            },{
+                xtype: 'kijs.gui.Button',
+                caption: 'valuesReset',
+                on: {
+                    click: function(e) {
+                        kijs.Array.each(this._content.elements, function(el) {
+                            if (el instanceof kijs.gui.field.Field) {
+                                el.valuesReset();
+                            }
+                        }, this);
+                    },
+                    context: this
+                }
+            }
+        ];
+    }
+
+    // Zeigt beim Übergebenen Feld einen isDirty-Button an, wenn etwas geändert wurde
+    _updateIsDirtyButton(el, addRemove=null) {
+        let btn = el.down('isDirtyResetButton');
+        
+        // button erstellen
+        if (addRemove===true && !btn) {
+            btn = new kijs.gui.Button({
+                xtype: 'kijs.gui.Button',
+                name: 'isDirtyResetButton',
+                caption: 'isDirty',
+                tooltip: 'isDirty zurücksetzen',
+                style: { borderColor: '#ff8800' },
+                captionStyle: { color: '#ff8800' },
+                on: {
+                    click: (e) => {
+                        e.element.parent.isDirty = false;
+                        this._updateIsDirtyButton(el);
+                    },
+                    context: this
+                }
+            });
+            el.add(btn);
+        }
+        
+        // button ein-/ausblenden
+        if (btn) {
+            btn.visible = el.isDirty;
+        }
+        
+        // button entfernen
+        if (addRemove===false && btn) {
+            el.remove(btn);
+        }
+    }
+    
+    _updateProperty(propertyName, value) {
+        kijs.Array.each(this._content.elements, function(el) {
+            if (el instanceof kijs.gui.field.Field) {
+                el[propertyName] = value;
+            }
+        }, this);
+    }
+    
+    
+    // PRIVATE
+    // LISTENERS
+    #onInputForIsDirty(e) {
+        this._updateIsDirtyButton(e.element);
+    }
+    
+
+
+    // --------------------------------------------------------------
+    // DESTRUCTOR
+    // --------------------------------------------------------------
+    destruct() {
+        this._content = null;
+    }
+
+};
