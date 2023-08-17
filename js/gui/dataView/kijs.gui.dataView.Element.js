d090bdab853b2464773cc1d331ac1123584acf40
Gerhard Kipfer <gerhard.kipfer@netas.ch>
2023-08-17 14:59:23 +0200

    Aufräumen

---
+++ js/gui/dataView/kijs.gui.dataView.Element.js
@@ -0,0 +1,88 @@
+/* global kijs, this */
+
+// --------------------------------------------------------------
+// kijs.gui.dataView.Element
+// --------------------------------------------------------------
+kijs.gui.dataView.Element = class kijs_gui_dataView_Element extends kijs.gui.Container {
+
+
+    // --------------------------------------------------------------
+    // CONSTRUCTOR
+    // --------------------------------------------------------------
+    // overwrite
+    constructor(config={}) {
+        super(false);
+
+        this._dataRow = {};     // Verweis auf den Data-Datensatz
+        this._index = null;
+        this._selected = false;
+
+        this._dom.clsRemove('kijs-container');
+        this._dom.clsAdd('kijs-dataview-element');
+        this._dom.htmlDisplayType = 'html';
+
+        //this._dom.nodeAttributeSet('tabIndex', -1);
+        this._dom.nodeAttributeSet('draggable', true);
+
+        // Standard-config-Eigenschaften mergen
+        Object.assign(this._defaultConfig, {
+            // keine
+        });
+
+        // Mapping für die Zuweisung der Config-Eigenschaften
+        Object.assign(this._configMap, {
+            dataRow: true,
+            index: true,
+            selected: { target: 'selected' }
+        });
+
+        // Config anwenden
+        if (kijs.isObject(config)) {
+            config = Object.assign({}, this._defaultConfig, config);
+            this.applyConfig(config, true);
+        }
+    }
+
+
+
+    // --------------------------------------------------------------
+    // GETTERS / SETTERS
+    // --------------------------------------------------------------
+    get dataRow() { return this._dataRow; }
+    set dataRow(val) { this._dataRow = val; }
+
+    get index() { return this._index; }
+    set index(val) { this._index = val; }
+
+    get selected() { return this._dom.clsHas('kijs-selected'); }
+    set selected(val) {
+        if (val) {
+            this._dom.clsAdd('kijs-selected');
+        } else {
+            this._dom.clsRemove('kijs-selected');
+        }
+    }
+
+
+
+    // --------------------------------------------------------------
+    // DESTRUCTOR
+    // --------------------------------------------------------------
+    // overwrite
+    destruct(superCall) {
+        if (!superCall) {
+            // unrender
+            this.unrender(superCall);
+
+            // Event auslösen.
+            this.raiseEvent('destruct');
+        }
+
+        // Variablen (Objekte/Arrays) leeren
+        this._dataRow = null;
+
+        // Basisklasse entladen
+        super.destruct(true);
+    }
+    
+};
