08e141a7403d1d199ca5f0c834a89c9b04bfb5a2
Gerhard Kipfer <gerhard.kipfer@netas.ch>
2024-06-04 11:47:34 +0200

    kijs.gui.DataView Neuer selectType:"singleAndEmpty"

--- js/kijs.js
+++ js/kijs.js
@@ -17,7 +17,7 @@ window.kijs = class kijs {
     // --------------------------------------------------------------
     // STATIC GETTERS / SETTERS
     // --------------------------------------------------------------
-    static get version() { return '2.7.1'; }
+    static get version() { return '2.7.2'; }
     
     
     
