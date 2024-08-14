157ba539e0ebc2917f7bcbfee9c945e52dee2945
Gerhard Kipfer <gerhard.kipfer@netas.ch>
2024-08-14 11:35:19 +0200

    2.7.4

--- js/kijs.js
+++ js/kijs.js
@@ -17,7 +17,7 @@ window.kijs = class kijs {
     // --------------------------------------------------------------
     // STATIC GETTERS / SETTERS
     // --------------------------------------------------------------
-    static get version() { return '2.7.3'; }
+    static get version() { return '2.7.4'; }
     
     
     
